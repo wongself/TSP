4857e39fabd2909ecee004dbae7f553ea125b55a
王榨菜 <wongself@gmail.com>
2021-04-27 18:10:49 +0800

    Update TS.py

--- TS.py
+++ TS.py
@@ -2,73 +2,73 @@ import random
 import numpy as np
 import math
 
-city_num = 48  # Total number of cities
-city_loc = np.loadtxt('city_location.txt')  # List of city coordinates
-table_len = 15  # 禁忌表长度
-taboo_table = []
-
 
 def calculate_distance_matrix():
     distance_matrix = []
     for src in range(city_num):
         distance_each = []
         for dst in range(city_num):
-            dis = math.sqrt(
+            dist = math.sqrt(
                 pow(city_loc[src][1] - city_loc[dst][1], 2) +
                 pow(city_loc[src][2] - city_loc[dst][2], 2))
-            distance_each.append(dis)
+            distance_each.append(dist)
         distance_matrix.append(distance_each)
     # print(distance_matrix)
     return distance_matrix
 
 
-# 计算所有路径对应的距离
-def calculate_path_distance(distance_matrix, path_new):
-    dis_list = []
-    for each in path_new:
-        dis = 0
-        for j in range(city_num - 1):
-            dis = distance_matrix[each[j]][each[j + 1]] + dis
-        dis = distance_matrix[each[29]][each[0]] + dis  # 回家
-        dis_list.append(dis)
-    return dis_list
+def calculate_path_distance(distance_matrix, path_candidate):
+    distance_list = []
+    for path in path_candidate:
+        dist = 0
+        for via in range(city_num - 1):
+            dist = distance_matrix[path[via]][path[via + 1]] + dist
+        dist = distance_matrix[path[city_num - 1]][path[0]] + dist
+        distance_list.append(dist)
+    # print(distance_list)
+    min_distance_list = min(distance_list)
+    print(min_distance_list)
+    return min_distance_list, path_candidate[distance_list.index(min_distance_list)]
 
 
 # 寻找上一个最优路径对应的所有领域解
 def find_newpath(path_best):
     path_new = []
-    for i in range(1, city_num - 1):
-        for j in range(i + 1, city_num):
+    for front in range(0, city_num - 1):
+        for end in range(front + 1, city_num):
             path = path_best.copy()
-            path[i], path[j] = path[j], path[i]
+            path[front], path[end] = path[end], path[front]
             path_new.append(path)
     return path_new
 
 
-# ==========================================
-# 点对点距离矩阵
-distance_matrix = calculate_distance_matrix()
+# Settings
+city_num = 48  # Total number of cities
+city_loc = np.loadtxt('city_location.txt')  # List of city coordinates
+iter_num = 100  # Iteration number
+table_len = 20  # Tabu table length
+tabu_table = []
 
-# 设置初始解
-path_initial = []
-initial = list(range(city_num))
-random.shuffle(initial)
-path_initial.append(initial)
-print(path_initial)
+# Path Initialization
+distance_matrix = calculate_distance_matrix()
+path_init = []
+permutation_init = list(range(city_num))
+random.shuffle(permutation_init)
+path_init.append(permutation_init)
 
 # 加入禁忌表
-taboo_table.append(initial)
+tabu_table.append(permutation_init)
 
 # 求初始解的路径长度
-dis_list = calculate_path_distance(distance_matrix, path_initial)
+dis_list = calculate_path_distance(distance_matrix, path_init)
 dis_best = min(dis_list)  # 最短距离
-path_best = path_initial[dis_list.index(dis_best)]  # 对应的最短路径方案
+path_best = path_init[dis_list.index(dis_best)]  # 对应的最短路径方案
 # print(path_best)
 
 # 初始期望
 expect_dis = dis_best
 expect_best = path_best
-for iter in range(5000):  # 迭代
+for iter in range(iter_num):  # 迭代
     # 寻找全领域新解
     path_new = find_newpath(path_best)
     # print(path_new)
@@ -83,22 +83,23 @@ for iter in range(5000):  # 迭代
     if dis_best < expect_dis:  # 最短的<期望
         expect_dis = dis_best
         expect_best = path_best  # 更新两个期望
-        if path_best in taboo_table:
-            taboo_table.remove(path_best)
-            taboo_table.append(path_best)
+        if path_best in tabu_table:
+            tabu_table.remove(path_best)
+            tabu_table.append(path_best)
         else:
-            taboo_table.append(path_best)
+            tabu_table.append(path_best)
     else:  # 最短的还是不能改善期望
-        if path_best in taboo_table:  # 在禁忌表里
+        if path_best in tabu_table:  # 在禁忌表里
             dis_new.remove(dis_best)
             path_new.remove(path_best)
             dis_best = min(dis_new)  # 求不在禁忌表中的最短距离
             path_best = path_new[dis_new.index(dis_best)]  # 对应的最短路径方案
-            taboo_table.append(path_best)
+            tabu_table.append(path_best)
         else:  # 不在禁忌表
-            taboo_table.append(path_best)
-    if len(taboo_table) >= table_len:
-        del taboo_table[0]
+            tabu_table.append(path_best)
+    if len(tabu_table) >= table_len:
+        del tabu_table[0]
 
-print('最短距离', expect_dis)
-print('最短路径：', expect_best)
+print('Initial path:', permutation_init)
+print('Shortest distance:', expect_dis)
+print('Shortest path:', expect_best)
