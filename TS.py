24b14c63d82a55a3ab00712f136616ff22baa856
王榨菜 <wongself@gmail.com>
2021-04-27 19:48:48 +0800

    Update TS.py

--- TS.py
+++ TS.py
@@ -3,6 +3,7 @@ import numpy as np
 import math
 
 
+# Distance matrix between two cities
 def calculate_distance_matrix():
     distance_matrix = []
     for src in range(city_num):
@@ -17,6 +18,7 @@ def calculate_distance_matrix():
     return distance_matrix
 
 
+# Distance corresponding to all paths
 def calculate_path_distance(distance_matrix, path_candidate):
     distance_list = []
     for path in path_candidate:
@@ -25,14 +27,12 @@ def calculate_path_distance(distance_matrix, path_candidate):
             dist = distance_matrix[path[via]][path[via + 1]] + dist
         dist = distance_matrix[path[city_num - 1]][path[0]] + dist
         distance_list.append(dist)
-    # print(distance_list)
-    min_distance_list = min(distance_list)
-    print(min_distance_list)
-    return min_distance_list, path_candidate[distance_list.index(min_distance_list)]
+    print(min(distance_list))
+    return distance_list
 
 
-# 寻找上一个最优路径对应的所有领域解
-def find_newpath(path_best):
+# All domain solutions corresponding to the current optimal path
+def geberate_neighbor_path(path_best):
     path_new = []
     for front in range(0, city_num - 1):
         for end in range(front + 1, city_num):
@@ -42,7 +42,7 @@ def find_newpath(path_best):
     return path_new
 
 
-# Settings
+# Setting
 city_num = 48  # Total number of cities
 city_loc = np.loadtxt('city_location.txt')  # List of city coordinates
 iter_num = 100  # Iteration number
@@ -52,36 +52,29 @@ tabu_table = []
 # Path Initialization
 distance_matrix = calculate_distance_matrix()
 path_init = []
-permutation_init = list(range(city_num))
-random.shuffle(permutation_init)
-path_init.append(permutation_init)
+sequence_init = list(range(city_num))
+random.shuffle(sequence_init)
+path_init.append(sequence_init)
+tabu_table.append(sequence_init)
 
-# 加入禁忌表
-tabu_table.append(permutation_init)
+# Initial Path Length
+dist_list = calculate_path_distance(distance_matrix, path_init)
+dist_best = min(dist_list)  # 最短距离
+path_best = path_init[dist_list.index(dist_best)]  # 对应的最短路径方案
+print(path_init)
 
-# 求初始解的路径长度
-dis_list = calculate_path_distance(distance_matrix, path_init)
-dis_best = min(dis_list)  # 最短距离
-path_best = path_init[dis_list.index(dis_best)]  # 对应的最短路径方案
-# print(path_best)
-
-# 初始期望
-expect_dis = dis_best
+# Initial Expectation
+expect_dist = dist_best
 expect_best = path_best
+
 for iter in range(iter_num):  # 迭代
-    # 寻找全领域新解
-    path_new = find_newpath(path_best)
-    # print(path_new)
-
-    # 求出所有新解的路径长度
-    dis_new = calculate_path_distance(distance_matrix, path_new)
-    # print(dis_new)
-
+    path_new = geberate_neighbor_path(path_best)  # 寻找全领域新解
+    dist_new = calculate_path_distance(distance_matrix, path_new)  # 寻找全领域新解
+    dist_best = min(dist_new)  # 最短距离
+    path_best = path_new[dist_new.index(dist_best)]  # 对应的最短路径方案
     # 选择路径
-    dis_best = min(dis_new)  # 最短距离
-    path_best = path_new[dis_new.index(dis_best)]  # 对应的最短路径方案
-    if dis_best < expect_dis:  # 最短的<期望
-        expect_dis = dis_best
+    if dist_best < expect_dist:  # 最短的<期望
+        expect_dist = dist_best
         expect_best = path_best  # 更新两个期望
         if path_best in tabu_table:
             tabu_table.remove(path_best)
@@ -90,16 +83,16 @@ for iter in range(iter_num):  # 迭代
             tabu_table.append(path_best)
     else:  # 最短的还是不能改善期望
         if path_best in tabu_table:  # 在禁忌表里
-            dis_new.remove(dis_best)
+            dist_new.remove(dist_best)
             path_new.remove(path_best)
-            dis_best = min(dis_new)  # 求不在禁忌表中的最短距离
-            path_best = path_new[dis_new.index(dis_best)]  # 对应的最短路径方案
+            dist_best = min(dist_new)  # 求不在禁忌表中的最短距离
+            path_best = path_new[dist_new.index(dist_best)]  # 对应的最短路径方案
             tabu_table.append(path_best)
         else:  # 不在禁忌表
             tabu_table.append(path_best)
     if len(tabu_table) >= table_len:
         del tabu_table[0]
 
-print('Initial path:', permutation_init)
-print('Shortest distance:', expect_dis)
+print('Initial path:', sequence_init)
+print('Shortest distance:', expect_dist)
 print('Shortest path:', expect_best)
